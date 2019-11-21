aa0c9823f49dd573039830bfa64d8c5762d7972e
michiwoo <michiwoo.web@gmail.com>
2019-11-20 23:50:13 -0600

    9. Usar Informacion real de las categorias

--- src/GlobalStyles.js
+++ src/GlobalStyles.js
@@ -35,7 +35,7 @@ export const GlobalStyle = createGlobalStyle`
   #app {
     box-shadow: 0 0 10px rgba(0, 0, 0, .05);
     overflow-x: hidden;
-    min-height: 100vp;
+    min-height: 100vh;
     padding-bottom: 10px;
   }
 `
