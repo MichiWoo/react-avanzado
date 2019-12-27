3a93523410524cb1e0048d81d2f4fc165a267349
michiwoo <michiwoo.web@gmail.com>
2019-12-26 22:15:05 -0600

    23. Refactorizando y usando variables de loading y error.

--- src/container/PhotoCardWithQuery.js
+++ src/container/PhotoCardWithQuery.js
@@ -3,7 +3,7 @@ import { PhotoCard } from '../components/photoCard'
 import { gql } from 'apollo-boost'// permite hacer las queries como si fuera un string
 import { Query } from 'react-apollo'// permite utilizar render props
 
-const query = gql`
+const GET_SINGLE_PHOTO = gql`
 query getSinglePhoto($id: ID!){
   photo(id: $id){
     id
@@ -15,18 +15,18 @@ query getSinglePhoto($id: ID!){
   }
 }
 `
+const renderProp = ({ loading, error, data }) => {
+  if (loading) return <p>Loading...</p>
+  if (error) return <p>Error...</p>
+  const { photo = {} } = data
+  return <PhotoCard {...photo} />
+}
 
 export const PhotoCardWithQuery = ({ id }) => {
   console.log('ID', id)
   return (
-    <Query query={query} variables={{ id }}>
-      {
-        ({ loading, error, data }) => {
-          if (loading) return null
-          const { photo = {} } = data
-          return <PhotoCard {...photo} />
-        }
-      }
+    <Query query={GET_SINGLE_PHOTO} variables={{ id }}>
+      {renderProp}
     </Query>
   )
 }
