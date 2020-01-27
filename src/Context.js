2250d0ed44c6ed2bff1e9c5ab101e8d0350ae3b9
ducter-dev <michel.morales@ducter.com.mx>
2020-01-26 23:29:01 -0600

    37. Persistiendo datos en el sessionStorage

--- src/Context.js
+++ src/Context.js
@@ -1,12 +1,19 @@
 import React, { useState, createContext } from 'react'
 
-const Context = createContext()
+export const Context = createContext()
 
 const Provider = ({ children }) => {
-  const [isAuth, setIsAuth] = useState(false)
+  const [isAuth, setIsAuth] = useState(() => {
+    return window.sessionStorage.getItem('token')
+  })
+
   const value = {
     isAuth,
-    activateAuth: () => setIsAuth(true),
+    activateAuth: token => {
+      console.log(token)
+      setIsAuth(true)
+      window.sessionStorage.setItem('token', token)
+    },
     deActivateAuth: () => setIsAuth(false)
   }
   return (
--- src/pages/NotRegisterUser.js
+++ src/pages/NotRegisterUser.js
@@ -1,47 +1,50 @@
 /* eslint-disable react/jsx-fragments */
 /* eslint-disable react/jsx-closing-tag-location */
 /* eslint-disable react/jsx-no-undef */
-import React, { Fragment } from 'react'
-import Context from '../Context'
+import React, { Fragment, useContext } from 'react'
+import { Context } from '../Context'
 import { UserForm } from '../components/UseForm'
 import { RegisterMutation } from '../container/RegisterMutation'
 import { LoginMutation } from '../container/LoginMutation'
 
-export const NotRegisterUser = () => (
-  <Context.Consumer>
-    {
-      ({ activateAuth }) => {
-        return <Fragment>
-          <RegisterMutation>
-            {
-              (register, { data, loading, error }) => {
-                const onSubmit = ({ email, password }) => {
-                  const input = { email, password }
-                  const variables = { input }
-                  register({ variables }).then(activateAuth)
-                }
+export const NotRegisterUser = () => {
+  const { activateAuth } = useContext(Context)
+  return <Fragment>
+    <RegisterMutation>
+      {
+        (register, { data, loading, error }) => {
+          const onSubmit = ({ email, password }) => {
+            const input = { email, password }
+            const variables = { input }
+            register({ variables }).then(({ data }) => {
+              console.log(data)
+              const { signup } = data
+              activateAuth(signup)
+            })
+          }
 
-                const errorMsg = error && 'El usuario ya existe o hay alún problema.'
-                return <UserForm disabled={loading} error={errorMsg} title='Registrarse' onSubmit={onSubmit} />
-              }
-            }
-          </RegisterMutation>
-          <LoginMutation>
-            {
-              (login, { data, loading, error }) => {
-                const onSubmit = ({ email, password }) => {
-                  const input = { email, password }
-                  const variables = { input }
-                  login({ variables }).then(activateAuth)
-                }
-
-                const errorMsg = error && 'La contraseña no es correcta o el usuario no existe.'
-                return <UserForm disabled={loading} error={errorMsg} title='Iniciar Sesión' onSubmit={onSubmit} />
-              }
-            }
-          </LoginMutation>
-        </Fragment>
+          const errorMsg = error && 'El usuario ya existe o hay alún problema.'
+          return <UserForm disabled={loading} error={errorMsg} title='Registrarse' onSubmit={onSubmit} />
+        }
       }
-    }
-  </Context.Consumer>
-)
+    </RegisterMutation>
+    <LoginMutation>
+      {
+        (login, { data, loading, error }) => {
+          const onSubmit = ({ email, password }) => {
+            const input = { email, password }
+            const variables = { input }
+            login({ variables }).then(({ data }) => {
+              console.log(data)
+              const { login } = data
+              activateAuth(login)
+            })
+          }
+
+          const errorMsg = error && 'La contraseña no es correcta o el usuario no existe.'
+          return <UserForm disabled={loading} error={errorMsg} title='Iniciar Sesión' onSubmit={onSubmit} />
+        }
+      }
+    </LoginMutation>
+  </Fragment>
+}
