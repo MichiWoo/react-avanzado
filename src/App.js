bd220d599e4bbfc2dc6ac2d11908184aea9e7386
ducter-dev <michel.morales@ducter.com.mx>
2020-02-23 23:36:16 -0600

    42. Ultimos retoques a las rutas.

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 /* eslint-disable react/jsx-closing-tag-location */
 /* eslint-disable react/jsx-fragments */
-import React from 'react'
-import { Router } from '@reach/router'
-import Context from './Context'
+import React, { useContext } from 'react'
+import { Router, Redirect } from '@reach/router'
+import { Context } from './Context'
 
 import { GlobalStyle } from './styles/GlobalStyles'
 
 import { User } from './pages/User'
 import { NotRegisterUser } from './pages/NotRegisterUser'
+import { NotFound } from './pages/NotFound'
 
 import { Logo } from './components/Logo'
 import { NavBar } from './components/NavBar'
@@ -16,30 +17,22 @@ import { Detail } from './pages/Detail'
 import { Favs } from './pages/Favs'
 
 export const App = () => {
+  const { isAuth } = useContext(Context)
   return (
     <div>
       <GlobalStyle />
       <Logo />
       <Router>
+        <NotFound default />
         <Home path='/' />
         <Home path='/pet/:id' />
         <Detail path='/detail/:detailId' />
+        {!isAuth && <NotRegisterUser path='/login' />}
+        {!isAuth && <Redirect from='/favs' to='/login' />}
+        {isAuth && <Redirect from='/login' to='/' />}
+        <Favs path='/favs' />
+        <User path='/user' />
       </Router>
-
-      <Context.Consumer>
-        {
-          ({ isAuth }) =>
-            isAuth
-              ? <Router>
-                <Favs path='/favs' />
-                <User path='/user' />
-              </Router>
-              : <Router>
-                <NotRegisterUser path='favs' />
-                <NotRegisterUser path='user' />
-              </Router>
-        }
-      </Context.Consumer>
       <NavBar />
     </div>
   )
