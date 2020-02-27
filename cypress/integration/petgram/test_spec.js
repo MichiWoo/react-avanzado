8c9ff5689095a705f3ec341752455d4c0c478941
Michiwoo <michiwoo.web@gmail.com>
2020-02-26 23:20:48 -0600

    48. Trsting con Cypress

---
+++ cypress/integration/petgram/test_spec.js
@@ -0,0 +1,25 @@
+/* global cy, describe, it */
+
+describe('Petgram', () => {
+  it('está funcionando', () => {
+    cy.visit('/')
+  })
+
+  it('permite navegar a una categoría y ver fotografías', () => {
+    cy.visit('/pet/1')
+    cy.wait(25)
+    cy.get('article')
+  })
+
+  it('permite navegar mediante navbar hasta la "home" de la app', () => {
+    cy.visit('/pet/1')
+    cy.wait(25)
+    cy.get('nav a').first().click()
+    cy.url().should('include', '/')
+  })
+
+  it('muestra el formulario de registro e inicio de sesión a los usuarios no autenticados cuando navegan a "favs"', () => {
+    cy.visit('/favs')
+    cy.get('form', { time: 100 }).should('have.length', 2)
+  })
+})
